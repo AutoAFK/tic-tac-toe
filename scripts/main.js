8c7c105acf29a8c8ab03562f54e4e7aa3a48656a
AutoAFK <ranhaim85@gmail.com>
2026-04-03 01:26:07 +0300

    Player actions as clousure

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+function player(name, playerSign) {
+  if (typeof name !== "string" || typeof playerSign !== "string") {
+    throw Error("Player name and sign need to be provided as strings.");
+  }
+  let wins = 0;
+
+  function playTurn(buttonElement) {
+    if (buttonElement.textContent !== "") {
+      buttonElement.textContent = playerSign;
+    }
+  }
+
+  return {playTurn}
+}
+
